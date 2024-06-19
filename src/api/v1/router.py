925f140c4e7886015f24928708040fc382bd9e69
povarna <povarna@adobe.com>
2024-06-19 11:42:23 +0300

    Project structure

--- src/api/v1/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from api.v1.routes import status
-
-router = APIRouter()
-
-router.include_router(router=status.router, tags=["Status"], prefix="/status")
--- src/core/models/status.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Status(BaseModel):
-    success: bool
-    version: str
-    message: str

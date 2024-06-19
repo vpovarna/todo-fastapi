a6c4e3b607ff29682cf4f0fdfb9a3d0395347622
povarna <povarna@adobe.com>
2024-06-19 23:47:47 +0300

    Adding Task Not Found error handler

--- src/api/v1/routes/tasks.py
+++ src/api/v1/routes/tasks.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
-from starlette.status import HTTP_201_CREATED, HTTP_200_OK
+from starlette.exceptions import HTTPException
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_404_NOT_FOUND
 
 from db.repositories.tasks import TasksRepository, get_tasks_repository
 from domain.response import Response
@@ -19,13 +20,17 @@ def create_task(create_task: TaskItemCreate, tasks_repo: TasksRepository = Depen
     return Response(data=Task(id=db_task.id), message="Task created successfully", status_code=HTTP_201_CREATED)
 
 
-@router.get("/{task_id}", response_model=Response[Task], status_code=HTTP_200_OK)
+@router.get("/{task_id}", response_model=Response[TaskInDB], status_code=HTTP_200_OK)
 def get_task(task_id: int, tasks_repo: TasksRepository = Depends(get_tasks_repository)) -> Response:
     """"
     Retrieves a task by `task_id`.
     """
 
     db_task = tasks_repo.get_task(task_id)
+
+    if db_task is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Task not found")
+
     task_in_db = TaskInDB(
         id=db_task.id,
         title=db_task.title,
@@ -33,6 +38,7 @@ def get_task(task_id: int, tasks_repo: TasksRepository = Depends(get_tasks_repos
         completed=db_task.completed,
         completed_at=db_task.completed_at
     )
+    print(task_in_db)
     return Response(data=task_in_db, status_code=HTTP_200_OK)
 
 # @router.delete("/{task_id")
